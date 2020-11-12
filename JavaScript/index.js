92b50c2758211d10c11bc967749c8572d6800ba1
Ruben <rbnsilogmail.com>
2020-11-11 22:28:33 -0600

    EStoy agregando animacion con javascript. Nota: Las animaciones seran con el dom, ya que seran muy pocas y snecillas. Ademas estara combinado con css

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,3 +1,38 @@
+const text = document.getElementById("fancy")
+const strText = text.textContent; //content of the text
+const splitText = strText.split("")
+text.textContent = ""; // limpio el contenido para que no se vena doble por mi looṕ
+
+
+const splitTextFilter = splitText.filter(function(letter){
+   if (letter != " " && letter != "\n") {
+     return letter
+   }  
+})
+
+for(let i = 0; i < splitTextFilter.length; i++){
+  text.innerHTML += "<span>" + splitTextFilter[i] + "</span>" // se repite eñ elemento. Por eso es que lo borro
+}
+
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick(){
+  const span = text.querySelectorAll('span')[char];
+  span.classList.add('fade');
+  char++ 
+  if(char === splitTextFilter.length){
+    complete();
+    return;
+  }
+}
+
+function complete(){
+  clearInterval(timer); 
+  timer = null;
+}
+
 $('.carousel').carousel({
     interval: 2500
   })
+
