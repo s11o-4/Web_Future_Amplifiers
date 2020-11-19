5066edd27d188d06e5dff24270cf622da970aa1c
Ruben <rbnsilogmail.com>
2020-11-18 23:16:38 -0600

    Aregue la base para la tabla y el subtiutulo

---
+++ JavaScript/features.js
@@ -0,0 +1,34 @@
+// subtitulo
+const text = document.getElementById("subtitule")
+const strText = text.textContent; //content of the text
+
+const splitText = strText.split("")
+text.textContent = ""; // limpio el contenido para que no se vena doble por mi looṕ
+
+const splitTextFilter = splitText.filter(function(letter){
+   if (letter != " " && letter != "\n") {
+     return letter
+   }  
+})
+
+for(let i = 0; i < splitTextFilter.length; i++){
+  text.innerHTML += "<span class='span-sub'>" + splitTextFilter[i] + "</span>";// se repite eñ elemento. Por eso es que lo borro
+}
+
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick(){
+  const span = text.querySelectorAll('span')[char];
+  span.classList.add('fade');
+  char++ 
+  if(char === splitTextFilter.length){
+    complete();
+    return;
+  }
+}
+
+function complete(){
+  clearInterval(timer); 
+  timer = null;
+}
